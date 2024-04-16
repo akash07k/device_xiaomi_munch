e0b1e5f493732fb94be8c2bbe793e7c8dce5048c
Akash Kakkar <6381747+akash07k@users.noreply.github.com>
2024-04-16 20:14:04 +0530

    munch: Build with GMS unless set explicitly
    Change-Id: Iedae93b81eb65477340d2518e8a39450d15a1863

--- lineage_munch.mk
+++ lineage_munch.mk
@@ -14,6 +14,11 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 # Inherit from munch device
 $(call inherit-product, device/xiaomi/munch/device.mk)
 
+# Include GMS by default, but rely on environment variable just in case we don't want to build with GMS conditionally
+ifneq ($(NO_GMS),true)
+$(call inherit-product-if-exists, vendor/gapps/arm64/arm64-vendor.mk)
+endif
+
 PRODUCT_NAME := lineage_munch
 PRODUCT_DEVICE := munch
 PRODUCT_MANUFACTURER := Xiaomi
